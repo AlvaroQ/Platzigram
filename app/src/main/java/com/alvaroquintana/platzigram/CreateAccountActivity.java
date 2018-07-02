85878d9ab53a39b5560d49a0b9b9e90e4a7085d4
alvaroQ <alvaroqp@gmail.com>
2018-07-03 00:40:34 +0200

    first commit

---
+++ app/src/main/java/com/alvaroquintana/platzigram/CreateAccountActivity.java
@@ -0,0 +1,24 @@
+package com.alvaroquintana.platzigram;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+
+public class CreateAccountActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_create_account);
+        showToolbar(getString(R.string.create_account), true);
+
+    }
+
+    public void showToolbar(String title, Boolean upButton) {
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+        getSupportActionBar().setTitle(title);
+        getSupportActionBar().setDisplayHomeAsUpEnabled(upButton);
+    }
+}
